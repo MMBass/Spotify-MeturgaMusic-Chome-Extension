7c6bd92e7ef34bbe3ef0455b43ecfd0819fd1905
unknown <mendibass@gmail.com>
2023-09-05 03:23:09 +0300

    StartObserveSongChanges, enNumsCharsRegex

--- scripts/content.js
+++ scripts/content.js
@@ -1,5 +1,6 @@
 
 let lines = [];
+let currFirstLine = '';
 
 function waitForElm(selector) {
     return new Promise(resolve => {
@@ -7,22 +8,30 @@ function waitForElm(selector) {
             return resolve(document.querySelector(selector));
         }
 
-        const observer = new MutationObserver(mutations => {
+        const observer = new MutationObserver(() => {
             if (document.querySelector(selector)) {
                 resolve(document.querySelector(selector));
                 observer.disconnect();
             }
         });
 
-        observer.observe(document.body, {
+        observer.observe(document, {
             childList: true,
             subtree: true
         });
     });
 }
 
-waitForElm('.NiCdLCpp3o2z6nBrayOn').then((elm) => {
+waitForElm('.NiCdLCpp3o2z6nBrayOn').then(() => { beforeGetFullTrans() }); // wait for lyrics appears (didn't work without a promise)
+
+function beforeGetFullTrans() {
     lines = document.getElementsByClassName("NiCdLCpp3o2z6nBrayOn");
+    const enNumsCharsRegex = /^[a-zA-Z0-9!@#$%^&*()-_=+[\]{}|;:'",.<>/? ]*$/;
+
+    for (elm of lines) {
+        if (!enNumsCharsRegex.test(elm.textContent)) return;
+    }
+   
     const linesText = [];
 
     for (elm of lines) {
@@ -30,19 +39,17 @@ waitForElm('.NiCdLCpp3o2z6nBrayOn').then((elm) => {
 
         if (elm.innerText.length > 3) elm.innerHTML +=
             `<br>
-                 <span class='transLoader'> 
-                    <span class='InnerTransLoader'> 
-                      ...טוען תרגום 
+                <span class='transLoader'> 
+                    <span class='InnerTransLoader' direction=rtl> 
+                        ...טוען תרגום 
                     </span>
-                 </span>
+                </span>
             </br>
             </br>`;
 
     }
     getFullTrans(linesText); // todo filter if not english, empty, and more
-});
-
-// todo observe if first line of NiCdLCpp3o2z6nBrayOn text changes / to another song (always same URL)
+}
 
 const serverUri = ''; // the translation should be private
 
@@ -62,12 +69,22 @@ const getFullTrans = (src, index) => {
         .then(response => response.json())
         .then(data => {
             if (data?.trans.length) {
+                currFirstLine = lines[0].textContent;
                 setLines(data.trans);
             }
         }
         ).catch((e) => {
-            // todo - alert "translation error" and remove the 'טוען תרגום..' spans
             console.log(e);
+            let i = 0;
+            for (elm of lines) {
+                if (elm.getElementsByClassName("transLoader")[0]) {
+                    elm.getElementsByClassName("transLoader")[0].innerHTML = `
+                        <span class='InnerTrans'> 
+                    
+                        <span>`;
+                }
+                i++;
+            } // empty the loader if error
         });
 }
 
@@ -76,10 +93,25 @@ function setLines(transArray) {
     for (elm of lines) {
         if (elm.getElementsByClassName("transLoader")[0]) {
             elm.getElementsByClassName("transLoader")[0].innerHTML = `
-                <span class='InnerTransLoader'> 
+                <span class='InnerTrans' direction=rtl> 
                      ${transArray[i]}
                 <span>`;
         }
         i++;
     }
+    StartObserveSongChanges();
+}
+
+function StartObserveSongChanges() {
+    // wait for new song changes:
+    const titleChangeObserver = new MutationObserver(mutationsList => {
+        if (mutationsList[0].textContent != currFirstLine) beforeGetFullTrans();
+        titleChangeObserver.disconnect();
+    });
+
+    titleChangeObserver.observe(document.querySelectorAll(".Q2RPoHcoxygOoPLXLMww")[0], {
+        childList: true,
+        subtree: true,
+        characterData: true,
+    });
 }

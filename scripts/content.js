7d51a28c4332c4efc1726c71ecaa9ff4e489caeb
unknown <mendibass@gmail.com>
2024-01-29 14:12:33 +0200

    no single google trans version, lines style fixed

--- scripts/content.js
+++ scripts/content.js
@@ -39,75 +39,20 @@ function beforeGetFullTrans() {
         linesText.push({ src: elm.innerText });
 
         if (elm.innerText.length > 3) elm.innerHTML +=
-            `<br>
+            `
                 <span class='transLoader'> 
                     <span class='InnerTransLoader' direction=rtl> 
                         ...טוען תרגום 
                     </span>
                 </span>
-            </br>
-            </br>`;
-
+                <br>
+            `;
     }
-    loopGoogleApiTrans(linesText);
-    // getFullTrans(linesText); // todo filter if not english, empty, and more
+
+    getFullTrans(linesText); // todo filter if not english, empty, and more
 }
 
-const loopGoogleApiTrans = async (linesText) => {
-    let i = 0;
-    for (elm of lines) {
-        const translatedText = await translateText(linesText[i].src);
-        if (elm.getElementsByClassName("transLoader")[0]) {
-            elm.getElementsByClassName("transLoader")[0].innerHTML = `
-                <span class='InnerTrans' direction=rtl> 
-                     ${translatedText}
-                <span>`;
-        }
-        i++;
-    }
-    StartObserveSongChanges();
-}
-
-const translateText = async (text) => {
-    const url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=${'he'}&dt=t&q=${encodeURIComponent(
-      text
-    )}`;
-  
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-    
-      var translatedTexts = [];
-      if (data && data[0]) {
-        data[0].forEach((element) => {
-          translatedTexts.push(element[0]);
-        });
-        
-        return translatedTexts.join(" ");
-      } else {
-        throw new Error("Translation failed.");
-        return 'Field...'
-      }
-    } catch (error) {
-      console.error("Translation error:", error);
-      return 'Error...'
-    }
-  };
-
-const googleTranslateText = async (text) => {
-    const url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=${'he'}&dt=t&q=${encodeURIComponent(text)}`;
-
-    try {
-        const response = await fetch(url);
-        const data = await response.json();
-        return data[0][0];
-    } catch (error) {
-        console.error('Translation error:', error);
-        return 'Failed...'; // Return original text if translation fails
-    }
-};
-
-const serverUri = 'https://musicline-backend.vercel.app'; // the translation should be private
+const serverUri = 'https://musicline-backend.vercel.app';
 
 const getFullTrans = (src, index) => {
     fetch(`${serverUri}/trans/lines`, {
